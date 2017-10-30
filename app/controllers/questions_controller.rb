6d92c2671c7584f2ee54455c9da3be66039ebb5f
Graham Cottridge <codinggc@gmail.com>
2017-10-30 14:04:50 +0000

    add app code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,18 @@
 class QuestionsController < ApplicationController
   def answer
+    @question = params[:query]
+    @answer = ""
+    if @question == "I am going to work right now!"
+      @answer = ""
+    elsif @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 
   def ask
   end
 end
+
+  
